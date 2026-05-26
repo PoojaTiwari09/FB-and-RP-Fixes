ac618ccb6ecdaccb7f9ea9e39786887b7042eb43
Harshaa650 <munnuri.harshavardhan@relanto.ai>
2026-05-26 15:49:24 +0530

    cleared folder structure

--- apps/web/src/modules/m2/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as CallReviewerDashboard } from './components/CallReviewerDashboard';
-export { default as SmartTrackerDashboard } from './components/SmartTrackerDashboard';
-export { ThemeSpotterDashboard } from './components/theme-spotter/ThemeSpotterDashboard';
-
